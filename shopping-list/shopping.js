f1c91a0c868bd54ff383d1f976e06704cf09e14e
ramirors1 <ramirezr@protonmail.com>
2021-06-01 15:39:05 -0500

    combined file

---
+++ shopping-list/shopping.js
@@ -0,0 +1,37 @@
+const list = [
+    {
+        item: "Bananas",
+        qty: 5,  
+        price: 1.50 
+    },
+    {
+        item: "Eggs",
+        qty: "dozen",
+        price: 2.50
+    },
+    {
+        item: "Pizza rolls",
+        qty: "box",
+        price: 4.50
+    },
+    {
+        item: "Protien",
+        qty: 1,
+        price: 7
+    },
+    {
+        item: "Fruit",
+        qty: 5,
+        price: 2.50
+    },
+    {
+        itme: "Sandwich buns",
+        qty: 1,
+        price: 3
+    },
+    {
+        item: "Condiments",
+        qty: 2,
+        price: 4
+    }
+]
